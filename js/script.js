a33a23deb74634f4bf6b73c55ce881747920ac27
Nicolas Kvaternik <nicolaskvaternik@gmail.com>
2019-05-02 11:18:03 +0200

    fusion

--- js/script.js
+++ js/script.js
@@ -13,5 +13,18 @@ $(function(){
             items:1
         }
     }
-})
+  });
+  var valeur = 0;
+  // on appelle une fonction '.click' avec l'id #clickMe rattaché au bouton
+  $('.add').click(function () {
+    // La méthode '.val()' permet de récupérer ou de modifier une valeur (récupère la valeur du nombre de clique)
+    //  on passe l'incrémentation de 'valeur' à cette méthode en argument
+    $('.quantity').html(++valeur);
+  });
+  // on appelle une fonction '.click' avec l'id #clickMe rattaché au bouton
+  $('.supp').click(function () {
+    if ($('.quantity').html() > 0) {
+      $('.quantity').html(--valeur);
+    }
+  });
 });
